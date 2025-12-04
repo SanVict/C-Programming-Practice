444ccba7c73ff82c5e9a1a18ebfb9c4b53b32aaa
Sanjay S <sanjaysureshk6@gmail.com>
2025-12-04 18:40:01 +0530

    ADDING FIBONACCI SERIES

--- practice/Fibonacci_Series.c
+++ practice/Fibonacci_Series.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
-void main(){
+void main(){ 
     int n ;
     printf("Enter the value :");
     scanf("%d",&n);
-    int temp=0;int i;
+    int temp=0;
     int a=0;int b =1;
-    for(i=0;i<=n;i++){
+    printf("%d %d",a,b);
+    for(int i=0;i<=n;i++) {
         temp=a+b;
-        printf("%d ",temp);
+        printf(" %d ",temp);
         a=b;
         b=temp;
     }
